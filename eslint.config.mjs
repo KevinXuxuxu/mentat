501e78f57315ae7641447073d453ba7fcbfd7d96
David Pu <push0216@gmail.com>
2024-04-29 22:35:30 -0700

    add eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import airbnbConfig from "eslint-config-airbnb";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReactConfig,
+  airbnbConfig,
+  eslintConfigPrettier
+];
