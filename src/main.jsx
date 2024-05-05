89a41705476c78cb4aaedc01860db894c378aefa
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2024-05-04 17:34:38 -0700

    Fix netlify deploy issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import Mentat from '/src/Mentat.jsx'
+import Mentat from './mentat.jsx'
 
 ReactDOM.render(
   <React.StrictMode>
