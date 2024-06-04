04f26efecd8c86248d46ac60ead44a451199e669
Boyu Liu <boyu.liu47@gmail.com>
2024-06-04 10:34:42 +0800

    offsets and search seem ok

--- src/components/backend/vectorDB.js
+++ src/components/backend/vectorDB.js
@@ -1,4 +1,5 @@
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { VectorStoreRetrieverMemory } from "langchain/memory";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { Document } from "@langchain/core/documents";
 import { v4 as uuidv4 } from 'uuid';
@@ -21,27 +22,52 @@ class VectorDB {
         const vectorObjs = await this.indexedDB.object("embedding").getAll();
         const vectors = vectorObjs.map(v => v.vector);
         const documents = vectorObjs.map(v => {
-            return { pageContent: '', metadata: { id: v.id } }
+            return { pageContent: '', metadata: {
+                id: v.id,
+                offset: v.offset } }
         })
         this.db.addVectors(vectors, documents);
     }
 
+    async splitText(text, chunkSize, chunkOverlap) {
+        const splittedTexts = [];
+        const offsets = [];
+        let start = 0;
+        let end = chunkSize;
+        while (start < text.length) {
+            if (end > text.length) {
+                end = text.length;
+            }
+            const splittedText = text.substring(start, end);
+            splittedTexts.push(splittedText);
+            offsets.push([start, end]);
+            start += chunkSize - chunkOverlap;
+            end = start + chunkSize;
+        }
+        return { splittedTexts, offsets };
+    };
+
     async addMessage(message, id) {
-        const documents = await this.splitter.splitDocuments([
-            new Document({
-                pageContent: message,
+        // add messsage to vector db in session, and to indexDB for persistence.
+        const { splittedTexts, offsets } = await this.splitText(message, 1000, 200);
+        const documents = splittedTexts.map((splittedText, index) => {
+            return {
+                pageContent: splittedText,
                 metadata: {
-                    id: id
+                    id: id,
+                    offset: offsets[index]
                 }
-            })
-        ]);
+            };
+        });
         const vectors = await this.embedder.embedDocuments(documents.map(d => d.pageContent));
         this.db.addVectors(vectors, documents);
-        vectors.forEach((v) =>
+        console.log("Adding message to vector store and indexDB");
+        vectors.forEach((v, index) =>
             this.indexedDB.object('embedding').put({
-                uuid: uuidv4(),
-                id: id,
-                vector: v
+            uuid: uuidv4(),
+            id: id,
+            vector: v,
+            offset: offsets[index]
             })
         )
     }
@@ -56,13 +82,24 @@ class VectorDB {
     }
 
     async search(queryText) {
-        const res = await this.db.similaritySearch(queryText, 3);
-        const messages = await Promise.all(res.map(async (r) => {
+        const res = await this.db.similaritySearch(queryText, 6);
+        // debugger
+        // const res = await this.db.similaritySearchWithScore(queryText, 6);
+        // get the chunk of message that relates to the query text
+        const allMessages = await Promise.all(res.map(async (r) => {
             const id = r.metadata.id;
-            const history = await this.indexedDB.object('history').get(id);
-            return history.content;
+            const message = await this.indexedDB.object('history').get(id);
+            const offset = r.metadata.offset;
+            // debugger
+            return message.content.substring(offset[0], offset[1]);
         }))
-        return messages;
+        // remove duplicated messages; need to dedup since chunks can have overlapping text too. 
+        // const uniqueMessages = allMessages.filter((message, index, self) => {
+        //     return index === self.findIndex((m) => (
+        //         m.metadata.id === message.metadata.id
+        //     ));
+        // });
+        return allMessages;
     }
 }
 
