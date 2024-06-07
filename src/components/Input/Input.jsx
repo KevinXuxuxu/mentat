e19fb51eef245d5d5c26e757a7af03018af237f0
Boyu Liu <boyu.liu47@gmail.com>
2024-06-07 11:30:15 +0800

    cannot send empty message to assistant

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,18 +2,23 @@ import SolarRoundArrowUpBold from '../../icons/SolarRoundArrowUpBold.jsx';
 import SolarRoundArrowUpLineDuotone from "../../icons/SolarRoundArrowUpLineDuotone.jsx";
 
 export const Input = ({ chatEnabled, message, handleKeyPress, handleInputChange, handleSendMessage }) => {
+    const isMessageEmpty = message.trim() === '';
+
     return (
         <div className="flex h-20 w-full">
             {chatEnabled ? (
                 <div className="flex-grow self-center">
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full" value={message} onKeyPress={handleKeyPress} onChange={handleInputChange} />
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full" 
+                    value={message} 
+                    onKeyPress={isMessageEmpty ? null : handleKeyPress} 
+                    onChange={handleInputChange} />
                 </div>) : (
                 <div className="flex-grow self-center">
                     <input type="text" placeholder="Type here" className="input input-bordered w-full" value={message} disabled />
                 </div>)}
             {chatEnabled ? (
                 <div className="flex-fit self-center p-2">
-                    <SolarRoundArrowUpBold onClick={handleSendMessage} className="cursor-pointer" />
+                    <SolarRoundArrowUpBold onClick={isMessageEmpty ? null : handleSendMessage} className={`cursor-pointer ${isMessageEmpty ? 'text-gray-400' : ''}`} />
                 </div>) : (
                 <div className="flex-fit self-center p-2">
                     <SolarRoundArrowUpLineDuotone />
