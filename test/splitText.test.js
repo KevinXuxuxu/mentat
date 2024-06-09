87a152d8c7076f30624e280fed8ca60a50ef9d29
boyuliu <boyu.liu47@gmail.com>
2024-06-09 11:01:39 +0800

    revert prettier

--- test/splitText.test.js
+++ test/splitText.test.js
@@ -48,3 +48,62 @@ test('vectorDB splitText function test with other separators', async () => {
         [ 235, 275 ]
     ]);
 });
+// test text with empty separators in splitText function
+test("vectorDB splitText function test with empty separators", async () => {
+  const vectorDB = new VectorDB(null, null, 20, 5, []);
+  const testText = `This is a test textIt has multiple sentencesEach sentence should be split into separate chunksHere is another sentence    And another oneAnd yet another sentenceAnd a sentence with a periodAnd a sentence with a question markAnd a sentence with an exclamation mark`;
+  const splittedDocs = await vectorDB.splitter.splitText(testText);
+  const splittedTexts = splittedDocs.map((d) => d.doc);
+  const offsets = splittedDocs.map((d) => [d.start, d.end]);
+  expect(splittedTexts).toEqual([
+    "This is a test textIt has multiple sentencesEach sentence should be split into separate chunksHere is another sentence    And another oneAnd yet another sentenceAnd a sentence with a periodAnd a sentence with a question markAnd a sentence with an exclamation mark",
+  ]);
+  expect(offsets).toEqual([[0, 263]]);
+});
+
+// test text with a large chunkSize in splitText function
+test("vectorDB splitText function test with large chunk size", async () => {
+  const largeChunkSize = 100;
+  const vectorDB = new VectorDB(null, null, largeChunkSize, 5);
+  const  testText =
+  "This is a test text. It has multiple sentences. Each sentence should be split into separate chunks. Except if the chunkSize is large enough, then the entire text should be in one chunk.";
+  const splittedDocs = await vectorDB.splitter.splitText(testText);
+  const splittedTexts = splittedDocs.map((d) => d.doc);
+  const offsets = splittedDocs.map((d) => [d.start, d.end]);
+  expect(splittedTexts).toEqual([
+    'This is a test text. It has multiple sentences. Each sentence should be split into separate chunks.',
+  ' Except if the chunkSize is large enough, then the entire text should be in one chunk.'
+]);
+  expect(offsets).toEqual([ [0, 99],
+    [99, 185]]);
+});
+
+
+// test text with a large chunk overlap in splitText function
+test("vectorDB splitText function test with large chunk overlap", async () => {
+  const vectorDB = new VectorDB(null, null, 50, 30);
+  const testText = `Hi. How are you. This is a test text. It has multiple sentences. That's right. Each sentence should be split into separate chunks. You hear me? This is a test text. You hear me? It does have multiple sentences. You hear? If the chunkSize is large enough. You hear? Then the entire text should be in one chunk.`;
+  const splittedDocs = await vectorDB.splitter.splitText(testText);
+  const splittedTexts = splittedDocs.map((d) => d.doc);
+  const offsets = splittedDocs.map((d) => [d.start, d.end]);
+  expect(splittedTexts).toEqual([
+    'Hi. How are you. This is a test text.',
+    ' This is a test text. It has multiple sentences.',
+    " It has multiple sentences. That's right.",
+    ' Each sentence should be split into separate chunks.',
+    ' You hear me? This is a test text. You hear me?',
+    ' You hear me? It does have multiple sentences.',
+    ' You hear? If the chunkSize is large enough.',
+    ' You hear?',
+    ' Then the entire text should be in one chunk.'
+  ]);
+  expect(offsets).toEqual([  [ 0, 37 ],
+    [ 37, 85 ],
+    [ 85, 126 ],
+    [ 126, 178 ],
+    [ 178, 225 ],
+    [ 225, 271 ],
+    [ 271, 315 ],
+    [ 315, 325 ],
+    [ 325, 370 ]]);
+});
