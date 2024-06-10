a356b284c7a154d4ae622d6104e0d15298074f72
boyuliu <boyu.liu47@gmail.com>
2024-06-10 15:58:37 +0800

    shift+enter creates new line in input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -4,18 +4,48 @@ import SolarRoundArrowUpLineDuotone from "../../icons/SolarRoundArrowUpLineDuoto
 export const Input = ({ chatEnabled, message, handleKeyPress, handleInputChange, handleSendMessage }) => {
     const isMessageEmpty = message.trim() === '';
 
+    function handleKeyDown(event) {
+        if (event.key === 'Enter' && !event.shiftKey) {
+            // send message if enter is pressed and message is not empty
+            event.preventDefault();
+            if (!isMessageEmpty) {
+                handleSendMessage();
+            }
+        } else if (event.key === 'Enter' && event.shiftKey) {
+            event.preventDefault();
+            // Insert newline if shift+enter is pressed
+            const textarea = event.target;
+            const start = textarea.selectionStart;
+            const end = textarea.selectionEnd;
+            const value = textarea.value;
+            textarea.value = value.substring(0, start) + '\n' + value.substring(end);
+            textarea.selectionStart = textarea.selectionEnd = start + 1;
+        }
+    }
+
     return (
         <div class="flex h-20 w-full">
             {chatEnabled ? (
                 <div class="flex-grow self-center">
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full"
-                    value={message}
-                    onKeyPress={isMessageEmpty ? null : handleKeyPress}
-                    onChange={handleInputChange} />
-                </div>) : (
+                    <textarea
+                        placeholder="Type here"
+                        className="input input-bordered w-full"
+                        value={message}
+                        onChange={handleInputChange}
+                        onKeyDown={handleKeyDown}
+                    />
+                </div>
+            ) : (
                 <div class="flex-grow self-center">
-                    <input type="text" placeholder="Type here" class="input input-bordered w-full" value={message} disabled />
-                </div>)}
+                    <input
+                        type="text"
+                        placeholder="Type here"
+                        className="input input-bordered w-full"
+                        value={message}
+                        disabled
+                    />
+                </div>
+            )}
             {chatEnabled ? (
                 <div class="flex-fit self-center p-2">
                     <SolarRoundArrowUpBold onClick={isMessageEmpty ? null : handleSendMessage} className={`cursor-pointer ${isMessageEmpty ? 'text-gray-400' : ''}`} />
